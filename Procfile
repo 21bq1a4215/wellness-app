040c656c6b1437af846256fef0ab2f8b2fb4b0d9
Giselle Chacon <gisecn@gmail.com>
2020-09-06 15:16:21 +0000

    Deployment to heroku part 1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wellness_app.wsgi:application
--- wellness_app/settings.py
+++ wellness_app/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wellness-app-django.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -128,12 +128,25 @@ WSGI_APPLICATION = 'wellness_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Social Account Facebook
 SOCIALACCOUNT_PROVIDERS = \
